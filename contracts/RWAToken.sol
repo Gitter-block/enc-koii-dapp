4ebdf1663512cb697137bb2bc2aa60a48cdfbf79
Gitter-block <aaron.ong@zoho.com>
2025-01-10 18:49:28 +0800

    Update RWAToken.sol

--- contracts/RWAToken.sol
+++ contracts/RWAToken.sol
@@ -1,1 +1,24 @@
+pragma solidity ^0.8.0;
 
+contract RWAToken {
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function mint(address account, uint256 amount) external {
+        require(account != address(0), "Invalid address");
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+}
